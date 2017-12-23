c80001b8482f7e30287b28e2a6f1cff96ecea36f
Eric Van Johnson <eric@diegodev.com>
2017-12-23 07:59:31 -0800

    Adding some Dialog Flow workflows

--- config/wave.php
+++ config/wave.php
@@ -6,7 +6,7 @@ return [
     'name'              => 'WavePHP',
     'start'             => 'September 19th, 2018',
     'end'               => 'September 21st, 2018',
-    'venue'             => ' The Bahia Resort Hotel',
+    'venue'             => 'The Bahia Resort Hotel',
     'venue_address'     => '998 West Mission Bay Drive, San Diego, CA 92109',
     'venue_booking'     => 'http://bahiahotel.com/groups/DD2018/',
     'purchase'          => 'https://www.eventbrite.com/e/wavephp-tickets-36798671818?ref=ebtnebregn',
--- routes/botman.php
+++ routes/botman.php
@@ -11,9 +11,22 @@ $botman->middleware->received($dialogflow);
 
 $botman->hears('greet', function ($bot) {
     $bot->reply('Hello there.');
-    //$bot->startConversation(
-    //    new IntroductionConversation()
-    //);
 })->middleware($dialogflow);
+
+$botman->hears('info', function ($bot) {
+    $extras = $bot->getMessage()->getExtras();
+    $bot->reply($extras['apiReply']);
+    //$bot->reply('WavePHP will be from '.config('wave.start').' until '. config('wave.end').' at '.config('wave.venue') .
+    //'. Tickets can be purchased at this url => '. config('wave.purchase') .
+    //'. You can also reserve a room at the conference hotel at a discounted rate at this url => ' . config('wave.venue_booking') .'.');
+})->middleware($dialogflow);
+
+$botman->hears('speaker', function ($bot) {
+    $bot->reply('Speakers have not been announced yet. Be patient, we promise it will be worth it.');
+})->middleware($dialogflow);
+
+$botman->hears('help', function ($bot) {
+    $bot->reply('You can ask me a couple thing like "When is the conference?" or "Who is speaking?".');
+});
 
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
