87307d7d47d149487797a11c52345fe639509d6a
Eric Van Johnson <eric@diegodev.com>
2017-12-24 17:46:10 -0800

    Adding call for papers section

--- routes/botman.php
+++ routes/botman.php
@@ -14,15 +14,21 @@ $botman->hears('greet', function ($bot) {
 })->middleware($dialogflow);
 
 $botman->hears('info', function ($bot) {
-    $extras = $bot->getMessage()->getExtras();
-    $bot->reply($extras['apiReply']);
-    //$bot->reply('WavePHP will be from '.config('wave.start').' until '. config('wave.end').' at '.config('wave.venue') .
-    //'. Tickets can be purchased at this url => '. config('wave.purchase') .
-    //'. You can also reserve a room at the conference hotel at a discounted rate at this url => ' . config('wave.venue_booking') .'.');
+    $user = $bot->getUser();
+    //$extras = $bot->getMessage()->getExtras();
+    //$bot->reply($extras['apiReply']);
+    $bot->reply('WavePHP will be from '.config('wave.start').' until '. config('wave.end').' at '.config('wave.venue') .
+        '. Tickets can be purchased at this url => '. config('wave.purchase') .
+        '. You can also reserve a room at the conference hotel at a discounted rate at this url => ' . config('wave.venue_booking') .
+        '. Hope to see you there '. $user->getFirstName());
 })->middleware($dialogflow);
 
 $botman->hears('speaker', function ($bot) {
     $bot->reply('Speakers have not been announced yet. Be patient, we promise it will be worth it.');
+})->middleware($dialogflow);
+
+$botman->hears('cfp', function ($bot) {
+    $bot->reply('Call for papers will be opened sometime in January.');
 })->middleware($dialogflow);
 
 $botman->hears('help', function ($bot) {
